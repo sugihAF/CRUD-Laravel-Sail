c2cf89efb3ff283806f64e858e5a46c28beafa11
root <root@sugih-af>
2024-09-28 21:56:43 +0700

    Add Item Retrieval API

---
+++ app/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json(
+            $items
+        );
+    }
+}
